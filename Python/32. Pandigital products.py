968a214aa5a8eb7342c07646b7c7c3e6bc0970d5
Finaris <metorres@mit.edu>
2019-08-01 16:26:50 -0400

    Added solution for pandigital products.

---
+++ Python/32. Pandigital products.py
@@ -0,0 +1,42 @@
+"""
+We shall say that an n-digit number is pandigital if it
+makes use of all the digits 1 to n exactly once; for example,
+the 5-digit number, 15234, is 1 through 5 pandigital.
+
+The product 7254 is unusual, as the identity, 39 × 186 = 7254,
+containing multiplicand, multiplier, and product is 1 through 9 pandigital.
+
+Find the sum of all products whose multiplicand/multiplier/product
+identity can be written as a 1 through 9 pandigital.
+
+HINT: Some products can be obtained in more than one way
+so be sure to only include it once in your sum.
+
+"""
+
+
+def is_pandigital_product(n):
+    """ Determines if n is a pandigital product.
+
+    :param n: (int) The number of interest.
+    :return: (bool) True if the number is pandigital and False otherwise.
+    """
+    # Figure out the numbers needed from the factors
+    remaining_digits = {i for i in range(1, 10)} - {int(digit) for digit in str(n)}
+
+    # There are only two possibilities: 1/4 digit factors or 2/3 digits.
+    # Start with 1/4 digits.
+    for i in [i for i in range(1, 10)] + [i for i in range(10, 100)]:
+        if int(n/i) == n/i:
+            factor_one, factor_two = i, int(n/i)
+            if remaining_digits == {int(digit) for digit in str(factor_one)} | {int(digit) for digit in str(factor_two)}:
+                return True
+    return False
+
+
+if __name__ == "__main__":
+    total = 0
+    for i in range(1000, 10000):
+        if is_pandigital_product(i):
+            total += i
+    print(total)
