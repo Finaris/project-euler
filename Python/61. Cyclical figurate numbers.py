4ae930a4cc3977b35d48e6e31a71e87ffae9526c
Finaris <metorres@mit.edu>
2019-12-16 20:55:49 -0500

    Completed 61.

--- Python/61. Cyclical figurate numbers.py
+++ Python/61. Cyclical figurate numbers.py
@@ -26,6 +26,8 @@ heptagonal, and octagonal, is represented by a different number in the set.
 
 """
 
+import random
+
 from typing import Callable
 
 
@@ -99,22 +101,106 @@ def octagonal_numbers(n: int) -> set:
     return generate_numbers_up_to_n(n, lambda i: i*((3*i)-2))
 
 
-def find_cyclical_figurate_numbers():
-    # Generate all numbers that are needed for verification.
-    all_triangle_numbers = triangular_numbers(10000)
-    all_square_numbers = square_numbers(10000)
-    all_pentagonal_numbers = pentagonal_numbers(10000)
-    all_hexagonal_numbers = hexagonal_numbers(10000)
-    all_heptagonal_numbers = heptagonal_numbers(10000)
-    all_octagonal_number = octagonal_numbers(10000)
+def find_cyclical_figurate_numbers(low: int, high: int) -> dict:
+    """ Finds the largest set of cyclical numbers between low and high (inclusive).
 
-    # Aggregate all of the numbers together for a quick check.
-    all_numbers = None  # TODO fix this
+    :param low: (int) Low end of numbers sought.
+    :param high: (int) High end of numbers of sought.
+    :return: (dict) Assignment between number type and number.
+    """
+    # Check all provided numbers. Track visited numbers.
+    seen = set()
+    for i in range(low, high+1):
+        cyclical_figurate_number_assignment = find_cyclical_figurate_number_assignment(i, seen)
+        if cyclical_figurate_number_assignment and validate_chain(cyclical_figurate_number_assignment):
+            return cyclical_figurate_number_assignment
+    return dict()
 
 
+def find_cyclical_figurate_number_assignment(inp: int, seen: set, graph: dict = None, assignment: dict = None) -> dict:
+    """ Tries to find a maximizing cyclical figurate number assignment,
+        returning an empty dictionary if it cannot.
 
+    :param inp: (int) Input number to search from.
+    :param seen: (set) All seen numbers.
+    :param graph: (dict) The graph to pull from.
+    :param assignment: (dict) The existing assignment.
+    :return: (dict) Dictionary mapping assignment.
+    """
+    # Check if the graph is populated or not.
+    if graph is None:
+        graph = dict(ALL_NUMBERS)
+    if assignment is None:
+        assignment = dict()
+    if not graph:
+        return assignment
+
+    # Determine if the number has been seen yet or not, or if its valid.
+    if inp < 1000 or inp in assignment.values():
+        return dict()
+
+    # Search the graph for a new child. Make an assignment if possible.
+    #seen.add(inp)
+    for node, values in graph.items():
+        # If the input is found, try it out.
+        if inp in values:
+            # Make a new assignment and graph.
+            new_assignment = dict(assignment)
+            new_assignment[node] = inp
+            new_graph = dict(graph)
+            del new_graph[node]
+
+            # Try all new next numbers and evaluate.
+            last_two_digits = str(inp)[2:]
+            for i in range(10, 100):
+                next_number = int(last_two_digits + str(i))
+                complete_assignment = find_cyclical_figurate_number_assignment(next_number, seen,
+                                                                               new_graph, new_assignment)
+                if complete_assignment:
+                    return complete_assignment
+    return dict()
+
+
+def validate_chain(chain: dict) -> bool:
+    """ Determines whether or not the found assignment makes a chain.
+
+    :param chain: (dict) An assignment chain
+    :return: (bool) True if the assignment makes a chain.
+    """
+    # Iterate through the chain and try to find a chain.
+    chain_copy = dict(chain)
+    value = chain_copy.popitem()[1]
+    while chain_copy:
+        suffix = str(value)[2:]
+        for next_node, next_value in chain_copy.items():
+            prefix = str(next_value)[:2]
+            if suffix == prefix:
+                value = next_value
+                del chain_copy[next_node]
+                break
+        else:
+            return False
+    return True
+
+
+# Global variables which hold all of the numbers.
+ALL_TRIANGULAR_NUMBERS = triangular_numbers(10000)
+ALL_SQUARE_NUMBERS = square_numbers(10000)
+ALL_PENTAGONAL_NUMBERS = pentagonal_numbers(10000)
+ALL_HEXAGONAL_NUMBERS = hexagonal_numbers(10000)
+ALL_HEPTAGONAL_NUMBERS = heptagonal_numbers(10000)
+ALL_OCTAGONAL_NUMBERS = octagonal_numbers(10000)
+ALL_NUMBERS = {
+    "triangular": ALL_TRIANGULAR_NUMBERS,
+    "square": ALL_SQUARE_NUMBERS,
+    "pentagonal": ALL_PENTAGONAL_NUMBERS,
+    "hexagonal": ALL_HEXAGONAL_NUMBERS,
+    "heptagonal": ALL_HEPTAGONAL_NUMBERS,
+    "octagonal": ALL_OCTAGONAL_NUMBERS
+}
 
 if __name__ == "__main__":
-    inp = None
-    out = sum(inp)
-    print(out)
+    # Evaluate the output for 4-digit numbers.
+    number_assignments = find_cyclical_figurate_numbers(1000, 10000)
+    print(f"Assignment: {number_assignments}")
+    print(f"Sum: {sum(number_assignments.values())}")
